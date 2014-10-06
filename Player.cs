ea2cb2a9bcade3107f016c9e31f4b1591efb5939
leafi <leaf@leaf.sx>
2014-10-06 21:45:50 +0100

    just some chargen structure bullshit

--- Player.cs
+++ Player.cs
@@ -6,6 +6,52 @@ using System.Threading.Tasks;
 
 namespace lo_novo
 {
+    public struct PlayerTraits
+    {
+        public int Level;
+        public int LevelUpAvailable;
+
+        // physical
+        public int MainStrength;
+        public int MainDexterity;
+        public int MainCharisma;
+        public int MainIntelligence;
+        public int MainSpeed;
+
+        // weapon skill
+        public int WeaponBlade; // SPD cap
+        public int WeaponBlaster; // DEX cap
+        public int WeaponBlunt; // STR cap
+        public int WeaponWrestling; // STR cap
+
+        // thinking skillz
+        public int SkillBio; // INT cap
+        public int SkillEvade; // Max(DEX, SPD) cap
+        public int SkillPunt; // DEX
+        public int SkillDefend; // STR cap
+        public int SkillObservant; // Max(INT, SPD) cap
+        public int SkillLore; // INT cap
+        public int SkillDeduction; // INT cap
+        public int SkillCharm; // CHA cap
+        public int SkillIntimidate; // Min(STR, CHA) cap
+        public int SkillPersuasion; // Min(INT, CHA) cap
+        public int SkillRepair; // Max(DEX, INT)
+        public int SkillWeaponize; // Max(CHA, INT)
+
+        // secret, magic attributes
+        public int MagicTenacity;
+        public int MagicPower;
+        public int MagicControl;
+        public int MagicLuck;
+        public int MagicTaint;
+
+        // personality tracking
+        public int PersonalityHonest;
+        public int PersonalityAnnoying;
+        public int PersonalityAmenable;
+        public int PersonalityReckless;
+    }
+
     public class Player : INoun
     {
         public string Name;
@@ -14,6 +60,9 @@ namespace lo_novo
         public Room Room;
         public List<Thing> Inventory = new List<Thing>();
         public DefaultInventoryResponses DefaultInventoryResponses;
+
+        public PlayerTraits Traits;
+        public string Appearance;
 
         public void Tick() { }
 
